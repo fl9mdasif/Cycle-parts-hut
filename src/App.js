ddd7a6c4bdd1db9b394004806ad816484936a605
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2022-05-23 17:52:35 +0600

    change file location

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NotFound from './pages/Shared/NotFound'
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import SignUp from './pages/Login/SignUp';
-import Blog from './pages/Login/Blog/Blog';
+import Blog from './pages/Blog/Blog';
 import PrivateAuth from './pages/Login/PrivateAuth';
 
 function App() {
@@ -15,11 +15,12 @@ function App() {
     <div className="App ">
       <Navbar />
       <Routes>
+        {/* Public page */}
         <Route path="/" element={<Home />}> </Route>
         <Route path="/home" element={<Home />}> </Route>
         <Route path="/login" element={<Login />}> </Route>
         <Route path="/signup" element={<SignUp />}> </Route>
-        {/* private */}
+        {/* private Page*/}
         <Route path="/blog" element={<PrivateAuth><Blog /></PrivateAuth>}> </Route>
         <Route path='*' element={<NotFound />}></Route>
 
